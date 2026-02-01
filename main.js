18138e40044583b7a06476eef57d4c550c2d02ef
marhas <marhas@noreply.codeberg.org>
2026-02-01 17:34:58 +0100

    Center map based on adverts

--- main.js
+++ main.js
@@ -41,14 +41,46 @@ const calcColor = (value) => {
   return colorToString(color)
 }
 
+// See <https://stackoverflow.com/a/14231286>.
+const calcCoordinatesCenter = (coordinates) => {
+  if (coordinates.length <= 0) return [48.31150015320114, 14.288522]
+  if (coordinates.length === 1) return coordinates[0]
+
+  let x = 0;
+  let y = 0;
+  let z = 0;
+
+  for (const c of coordinates)
+  {
+      var latitude = c[0] * Math.PI / 180;
+      var longitude = c[1] * Math.PI / 180;
+
+      x += Math.cos(latitude) * Math.cos(longitude);
+      y += Math.cos(latitude) * Math.sin(longitude);
+      z += Math.sin(latitude);
+  }
+
+  var total = coordinates.length;
+
+  x = x / total;
+  y = y / total;
+  z = z / total;
+
+  var centralLongitude = Math.atan2(y, x);
+  var centralSquareRoot = Math.sqrt(x * x + y * y);
+  var centralLatitude = Math.atan2(z, centralSquareRoot);
+
+  return [centralLatitude * 180 / Math.PI, centralLongitude * 180 / Math.PI];
+}
+
 const main = () => {
   document.body.style.border = "5px solid red";
 
   // Map container.
-  let mapDiv = document.createElement("div");
+  const mapDiv = document.createElement("div");
   mapDiv.setAttribute("id", "map");
   document.body.prepend(mapDiv)
-  let map = L.map('map').setView([48.31150015320114, 14.288522], 10);
+  const map = L.map('map');
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
@@ -64,13 +96,14 @@ const main = () => {
   // Min/Max price per m².
   const [min, max] = [9, 15]
 
-  adverts
-    .forEach(advert => {
+  const preparedAdverts = adverts
+    .flatMap(advert => {
       const id = advert.id
-      const c = advert.attributes.attribute.filter(att => att.name == "COORDINATES").flatMap(x => x.values[0].split(","))
-      const title = advert.attributes.attribute.find(att => att.name == "HEADING").values[0]
-      const price = advert.attributes.attribute.find(att => att.name == "PRICE").values[0]
-      const address = advert.attributes.attribute.find(att => att.name == "ADDRESS")?.values[0]
+      const attr = advert.attributes.attribute
+      const c = attr.filter(att => att.name == "COORDINATES").flatMap(x => x.values[0].split(","))
+      const title = attr.find(att => att.name == "HEADING").values[0]
+      const price = attr.find(att => att.name == "PRICE").values[0]
+      const address = attr.find(att => att.name == "ADDRESS")?.values[0]
       const size = advert.teaserAttributes[0]?.value
       const pricePerSize = calcPricePerSize(price, size)
       const color = calcColor(normalize(pricePerSize, min, max))
@@ -78,10 +111,28 @@ const main = () => {
       console.log(id)
       if (c.length <= 0) {
         console.log("no coordinates", advert)
-        return
+        return []
       }
 
-      L.circle(c, { radius: 48, stroke: true, strokeWidth: 1, fill: true, fillColor: color, fillOpacity: 1.0 })
+      return [
+        {
+          id,
+          coordinates: c,
+          title,
+          price,
+          address,
+          size,
+          pricePerSize,
+          color
+        }
+      ]
+    })
+
+    const mapCenter = calcCoordinatesCenter(preparedAdverts.map(pa => pa.coordinates))
+    map.setView(mapCenter, 10)
+
+    preparedAdverts.forEach(({id, coordinates, color, title, price, size, pricePerSize, address}) => {
+      L.circle(coordinates, { radius: 48, stroke: true, strokeWidth: 1, fill: true, fillColor: color, fillOpacity: 1.0 })
         .bindPopup(`<b>${title}</b><br>\
           ${price}€, ${size}m², ${pricePerSize ?? "?"}€/m²<br>\
           ${address ?? "-"} <br>\
