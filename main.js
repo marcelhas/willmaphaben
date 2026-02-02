f0256ea8654cf4fd2cf3b6a15a35c67ce2c22cc7
marhas <marhas@noreply.codeberg.org>
2026-02-02 20:07:01 +0100

    Remove maxZoom from map

--- main.js
+++ main.js
@@ -43,7 +43,6 @@ const rerenderMap = (adverts) => {
 
   const map = L.map("map");
   L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    maxZoom: 19,
     attribution:
       '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
   }).addTo(map);
